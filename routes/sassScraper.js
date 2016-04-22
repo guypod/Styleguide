5b7387b355d3ec6a2e5b03a5c6922143854c0d4c
Kestutis Buivydas <kestutis.buivydas@devbridge.com>
2016-04-22 17:42:49 +0300

    Check if colors exist in scrapped file. Fixes #93

--- routes/sassScraper.js
+++ routes/sassScraper.js
@@ -208,7 +208,7 @@ var compareForReport = function(theme, report) {
   }
 
   report.themeName = theme.name;
-  report.uniqueColVals = Object.keys(theme.colors).length;
+  report.uniqueColVals = theme.colors ? Object.keys(theme.colors).length : 0;
   report.diffOfColVals = report.uniqueColVals - Object.keys(oldData.colors).length;
 
   if (theme.typography && !theme.typography.equals(oldData.typography)) {
